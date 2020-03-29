15898fc0ebf31badcb3860e5089c5bad01ce156a
Kathleen Graham <kathleeneliza.graham@gmail.com>
2020-03-29 14:35:29 -0500

    added main function to main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+fetch('https://api.adviceslip.com/advice')
+.then((response) => {
+    console.log('Response: ', response);
+
+    return response.json();
+})
+.then(data => {
+    const tweetURL = "https://twitter.com/intent/tweet?text=";
+    const adviceText = data.slip.advice;
+    const adviceElement = document.getElementById("advice");
+    const tweetButton = document.getElementById("tweet-button");
+
+    adviceElement.innerText = adviceText;
+    tweetButton.attributes.href.value = tweetURL + encodeURIComponent(adviceText);
+
+    console.log('API JSON Data: ', data);
+    console.log('Advice: ', adviceText)
+});
+
+console.log("Hello!");
